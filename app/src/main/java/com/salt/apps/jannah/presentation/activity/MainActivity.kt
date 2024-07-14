d822cedc0536992b6c88ccef172a0f2d31c7277a
Danu Firdaus <danufirdaus51@gmail.com>
2024-07-14 20:15:18 +0700

    Update main activity state

--- app/src/main/java/com/salt/apps/jannah/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/salt/apps/jannah/presentation/activity/MainActivity.kt
@@ -5,11 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.salt.apps.core.design.theme.JannahTheme
 import com.salt.apps.jannah.presentation.app.JannahApp
 import com.salt.apps.jannah.presentation.app.rememberAppState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -17,11 +27,18 @@ class MainActivity : ComponentActivity() {
     private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
 
-        installSplashScreen().apply {
-            setKeepOnScreenCondition {
-                mainViewModel.isLoading.value
+        var mainState: MainActivityState by mutableStateOf(MainActivityState.Loading)
+
+        splashScreen.setKeepOnScreenCondition {
+            mainViewModel.mainActivityStateFlow.value is MainActivityState.Loading
+        }
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.mainActivityStateFlow.onEach { mainState = it }.collect()
             }
         }
 
--- app/src/main/java/com/salt/apps/jannah/presentation/activity/MainViewModel.kt
+++ app/src/main/java/com/salt/apps/jannah/presentation/activity/MainViewModel.kt
@@ -1,24 +1,46 @@
 package com.salt.apps.jannah.presentation.activity
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.salt.apps.core.domain.usecase.AlQuranUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
-    private val _isLoading = MutableStateFlow(true)
-    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+class MainViewModel @Inject constructor(
+    private val alQuranUseCase: AlQuranUseCase
+) : ViewModel() {
 
-    init {
-        viewModelScope.launch {
-            delay(2000)
-            _isLoading.value = false
-        }
-    }
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val mainActivityStateFlow: StateFlow<MainActivityState> =
+        alQuranUseCase.getQuran().flatMapConcat { surahList ->
+            surahList.asFlow().flatMapMerge { surah ->
+                alQuranUseCase.getDetailSurah(
+                    surah.nomor,
+                )
+            }
+        }.map {
+            MainActivityState.Success
+        }.catch {
+            Log.e("ERROR", "$it")
+        }.stateIn(
+            scope = viewModelScope,
+            initialValue = MainActivityState.Loading,
+            started = SharingStarted.WhileSubscribed(10_000),
+        )
+}
+
+sealed interface MainActivityState {
+    data object Loading : MainActivityState
+    data object Success : MainActivityState
 }
